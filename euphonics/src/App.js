60b87912af04a8c8fb45b2258c5887964e9c8527
deeWooley <wooleymediallc@gmail.com>
2022-05-18 21:49:14 -0400

    Add React

---
+++ euphonics/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from '@mui/material'
+// import {} from '@material-ui/core'
+
+function App() {
+  return (
+    <div className="App">
+ <h1>WELCOME TO </h1>
+ <h1>Euphonics</h1>
+ <button>A Button</button>
+ <Button> MUI button </Button>
+    </div>
+  );
+}
+
+export default App;
