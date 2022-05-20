14727e3f8edc690aae977246acfe9f43984cc066
deeWooley <wooleymediallc@gmail.com>
2022-05-19 21:27:38 -0400

    styling

--- euphonics/src/App.js
+++ euphonics/src/App.js
@@ -1,17 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import imageLogo from '../src/static/logo.png'
+
 import { Button } from '@mui/material'
 // import {} from '@material-ui/core'
 
 function App() {
   return (
     <div className="App">
- <h1>WELCOME TO </h1>
- <h1>Euphonics</h1>
- <button>A Button</button>
- <Button> MUI button </Button>
+      
+ {/* <h1>WELCOME TO </h1> */}
+ {/* <h1>euphonics</h1> */}
+ <img src = {imageLogo} alt = "eupohonics logo" height = {40} id  = "logo"/>
+ 
+
+ {/* <button>A Button</button>
+ <Button> MUI button </Button> */}
     </div>
   );
+
+
+
+
+
 }
 
 export default App;
