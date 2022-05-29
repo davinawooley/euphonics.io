0558fe4874b19cb0d5b89148fa99aefce8c115ba
deeWooley <wooleymediallc@gmail.com>
2022-05-28 21:35:53 -0400

    UI update

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,15 @@ import React, {useState} from 'react';
 import '../App.css';
 import imageLogo from '../../src/static/logo.png'
 import imageSpotify from '../../src/static/spotify.png'
+import { Link } from "react-router-dom";
 
 export default function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
   return (
     
     <div>
      <a><h3 id = 'navigation'>About</h3></a>
+     {/* <nav><Link to = '/about'>tst</Link></nav> */}
     <img src = {imageLogo} alt = "eupohonics logo" height = {40} id  = "logo"/>
     
     <h1 id = 'title'>euphonics.io</h1>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import App from "./components/App";
+// import About from "./components/About";
+
 
 const container = document.getElementById("root");
 ReactDOM.hydrateRoot(container, <App />);
+
+
+
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById("root")
+// );
+// root.render(<App />);
