75168f4e90066f72af27f731706286d6cfbab5a7
deeWooley <wooleymediallc@gmail.com>
2022-05-29 22:49:39 -0400

    UI Update

--- src/components/App.js
+++ src/components/App.js
@@ -5,18 +5,15 @@ import imageSpotify from '../../src/static/spotify.png'
 import { Link } from "react-router-dom";
 
 export default function App() {
-  // const [count, setCount] = useState(0);
   return (
     
     <div>
      <a><h3 id = 'navigation'>About</h3></a>
-     {/* <nav><Link to = '/about'>tst</Link></nav> */}
     <img src = {imageLogo} alt = "eupohonics logo" height = {40} id  = "logo"/>
-    
     <h1 id = 'title'>euphonics.io</h1>
     <h2 id = 'subtitle'>The Hip-Hop Mental Health Playlist Generator</h2>
     <a><button id = 'accessButton' > <img src = {imageSpotify} alt = "spotify logo" height = {27} id = 'spotify'/><span>Access with Spotify</span></button></a>
-  <h6 id='footer'>© 2022 DEW</h6>
+    <h6 id='footer'>© 2022 DEW</h6>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,16 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
 import App from "./components/App";
-// import About from "./components/About";
 
 
 const container = document.getElementById("root");
 ReactDOM.hydrateRoot(container, <App />);
-
-
-
-
-// const root = ReactDOM.createRoot(
-//   document.getElementById("root")
-// );
-// root.render(<App />);
