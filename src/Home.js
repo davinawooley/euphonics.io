811dd51191888491360c04535985211dfd2ee373
deeWooley <wooleymediallc@gmail.com>
2022-06-04 16:58:12 -0400

    Modal basic function

--- src/Home.js
+++ src/Home.js
@@ -3,35 +3,56 @@ import imageLogo from "./static/logo.png";
 import imageSpotify from "./static/spotify.png";
 import './Home.css';
 import './About';
-
-
+import Modal from '@material-ui/core/Modal';
+  
 function Home() {
+  const [open, setOpen] = React.useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+    
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
   return (
     <div>
       <div id = "container">
-   
       <img src = {imageLogo} alt = "eupohonics logo" height = {40} id  = "logo"/>
-      <a href = './About'>  <h3 id = "navigation">
-    About</h3></a>
-
-
-
+      <a href = './About'>  <h3 id = "navigation">About</h3></a>
     </div>
 
-   
     <h1 id ="title">euphonics.io</h1>
     <p>The Hip-Hop Mental Health Playlist Generator</p>
-    <button id = "accessButton" >
-         <img src = {imageSpotify} alt = "spotify logo" height = {27} id = "spotify"/>
-         <span>Access with Spotify</span></button>
 
+    <button id = "accessButton" type="button" 
+      onClick={handleOpen}>
+        
+        <img src = {imageSpotify} alt = "spotify logo" height = {27} id = "spotify"/>
+         <span>Access with Spotify</span>
+      </button>
+      <Modal
+        onClose={handleClose}
+        open={open}
+        style={{
+          position: 'absolute',
+          backgroundColor: 'black',
+          borderRadius: 15,
+          padding: 3,
+          color:'white',
+          outline: 10,
+          borderColor: 'white',
+          border: '2px solid #983FE0',
+          height:250,
+          width: 500,
+          margin: 'auto'
+        }}
+      >
+        <h2>Username/Password</h2>
+      </Modal>
 
     <h6 id="footer">© 2022 DEW</h6>
-
     </div>
-
   );
-
 }
 export default Home;
-
---
+++ src/LoginModal.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import './LoginModal.css'
+
+// import imageLogo from '../src/static/logo.png'
