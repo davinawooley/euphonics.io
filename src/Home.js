3bc10208a5559da93c18fd41ccb1e20fc53cff30
deeWooley <wooleymediallc@gmail.com>
2022-06-09 00:44:50 -0400

    Spotify auth

--- src/Home.js
+++ src/Home.js
@@ -8,6 +8,12 @@ import { Helmet } from 'react-helmet';
   
 function Home() {
   const [open, setOpen] = React.useState(false);
+  const CLIENT_ID = "7e4ac27e10df469fa4de1530780b0a78"
+  // const CLIENT_SECRET = "7cea836af1da44539cd839dcf44413d7"
+  const REDIRECT_URI = "http://localhost:3000"
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+  
+  const RESPONSE_TYPE = "token"
   const handleClose = () => {
     setOpen(false);
   };
@@ -33,7 +39,7 @@ function Home() {
       >
       
       <img src = {imageSpotify} alt = "spotify logo" height = {27} id = "spotify"/>
-      <span>Access with Spotify</span>
+      <span><a href = {`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Access with Spotify</a></span>
       </button>
 
       <Modal
