af026e30f0fd9bf5cfc34790588f5224761eac51
deeWooley <wooleymediallc@gmail.com>
2022-06-11 16:21:57 -0400

    redirect spotify

--- src/Home.js
+++ src/Home.js
@@ -15,7 +15,7 @@ function Home() {
   // const [open, setOpen] = React.useState(false);
   // spotify auth clid uri and auth endpoint
   const CLIENT_ID = "7e4ac27e10df469fa4de1530780b0a78"
-  const REDIRECT_URI = "http://localhost:3000"
+  const REDIRECT_URI = "http://euphonics.io/emotions"
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
   
   const RESPONSE_TYPE = "token"
