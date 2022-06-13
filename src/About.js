cacb5956a1b64d1bfb7c4f84f441f46b9f6e37e0
deeWooley <wooleymediallc@gmail.com>
2022-06-12 22:07:40 -0400

    about styling

--- src/About.js
+++ src/About.js
@@ -20,8 +20,13 @@ function About() {
 
     <h1 id = 'title'>euphonics.io</h1>
     <div className = 'bodyTop'>
-    <button id = 'accessButtonD' > <img src = {imageLogo} alt = "euphonics logo" height = {27} id = 'spotify'/><span>Our Story</span></button>
+    <img src = {imageLogo} alt = "eupohonics logo" height = {40} id  = "logoBigLeft"/>
+       
 
+  
+  
+    <button id = 'accessButtonD' > <img src = {imageLogo} alt = "euphonics logo" height = {27} id = 'spotify'/><span>Our Story</span></button>
+   
     <h2 id = 'intro'>Euphonics.io implements an AI algorithms that analyzes user listening history and favorite artists to generate hip hop playlists. 
     <br/>
     Euphonics.io is a free web application that integrates with Spotify applications. 
@@ -33,42 +38,37 @@ function About() {
     </div>
 
     <div className = 'bodyTop'>
-    <button id = 'accessButtonD' > 
-    {/* <img src = {imageSpotify} alt = "spotify logo" height = {27} id = 'spotify'/> */}
-    <span>DISCLAIMER</span></button>
-   
-
+     <span id = 'disclaimTitle'>DISCLAIMER</span>
+    
     <h2 id = 'disclaim'> Euphonics.io is not a substitution to receiving medical attention. This application is meant to create personalized playlists to provide perspective to what users are navigating through to benefit users.</h2>
     </div>
 
 
     <div className = 'bodyTop'>
-    <button id = 'accessButtonD' > 
-    {/* <img src = {imageSpotify} alt = "spotify logo" height = {27} id = 'spotify'/> */}
-    <span>MENTAL HEALTH RESOURCES</span></button>
-   
+
+     <span id ='mhrTitle'>MENTAL HEALTH RESOURCES</span>
    
     <h2 id = 'disclaim'>    
-      National Suicide Prevention Hotline 
+      <u>National Suicide Prevention Hotline</u>
       <br/>1.800.273.8255
-      <br/>
-      National Suicide Prevention Lifeline (for Deaf and Hard of Hearing)
+      <br/><br/>
+      <u>National Suicide Prevention Lifeline (for Deaf and Hard of Hearing)</u>
       <br/>
       For TTY Users: Use relay service or dial 711 then 1.800.273.8255
-      <br/>
-      National Domestic Violence Hotline 
+      <br/><br/>
+      <u>National Domestic Violence Hotline</u>
       <br/>1.800.799.7233
-      <br/>
-      National Grad Crisis Line 
+      <br/><br/>
+      <u>National Grad Crisis Line</u>
       <br/>1.877.472.3457
-      <br/>
-      National Sexual Assault Hotline 
+      <br/><br/>
+      <u>National Sexual Assault Hotline</u>
       <br/>1.800.656.4673
       <br/><br/>
-      National Suicide Prevention Lifeline 
+      <u>National Suicide Prevention Lifeline </u>
       <br/>1.800.273.8255
-      <br/>
-      Substance Abuse and Mental Health Services Administration National Helpline 
+      <br/><br/>
+      <u>Substance Abuse and Mental Health Services Administration National Helpline</u> 
       <br/>1.800.662.4357
     </h2>
     </div>
