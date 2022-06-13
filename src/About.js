f23c88933a10da6ffe4633c41891865df08894d8
deeWooley <wooleymediallc@gmail.com>
2022-06-13 15:38:50 -0400

    about content

--- src/About.js
+++ src/About.js
@@ -18,29 +18,33 @@ function About() {
         <h3 id = 'navigation'>About</h3>
       </div>
 
-    <h1 id = 'title'>euphonics.io</h1>
-    <div className = 'bodyTop'>
-    <img src = {imageLogo} alt = "eupohonics logo" height = {40} id  = "logoBigLeft"/>
-       
 
-  
-  
-    <button id = 'accessButtonD' > <img src = {imageLogo} alt = "euphonics logo" height = {27} id = 'spotify'/><span>Our Story</span></button>
-   
-    <h2 id = 'intro'>Euphonics.io implements an AI algorithms that analyzes user listening history and favorite artists to generate hip hop playlists. 
+
+
+    <div className = 'bodyTop'>
+
+
+    <h1 class ="title" id = 'space'>euphonics.io</h1>
+
+
+    <div >
+
+    <img src = {imageLogo} alt = "eupohonics logo" height = {40} id  = "logoBigLeft"/>
+       </div>
+       <span id = 'disclaimTitle'>OUR STORY</span>
+    <h2 id = 'disclaim'>Euphonics.io implements an AI algorithms that analyzes user listening history and favorite artists to generate hip hop playlists. 
     <br/>
     Euphonics.io is a free web application that integrates with Spotify applications. 
     <br/>
     Euphonics.io is an application for the people. It is and will always be free. We do not store your personal information. 
     </h2>
-    <br/>
-    <br/>
-    </div>
 
-    <div className = 'bodyTop'>
+<br/>
+<br/>
+<br/>
      <span id = 'disclaimTitle'>DISCLAIMER</span>
     
-    <h2 id = 'disclaim'> Euphonics.io is not a substitution to receiving medical attention. This application is meant to create personalized playlists to provide perspective to what users are navigating through to benefit users.</h2>
+    <h2 id = 'disclaim'> Euphonics.io is not a substitution to receiving medical attention.<br/><br/> This application is meant to create personalized playlists to provide perspective to what users are navigating through to benefit users.</h2>
     </div>
 
 
@@ -49,31 +53,24 @@ function About() {
      <span id ='mhrTitle'>MENTAL HEALTH RESOURCES</span>
    
     <h2 id = 'disclaim'>    
-      <u>National Suicide Prevention Hotline</u>
+    <a href = "https://suicidepreventionlifeline.org/"><u>National Suicide Prevention Hotline</u></a>
       <br/>1.800.273.8255
       <br/><br/>
-      <u>National Suicide Prevention Lifeline (for Deaf and Hard of Hearing)</u>
-      <br/>
-      For TTY Users: Use relay service or dial 711 then 1.800.273.8255
-      <br/><br/>
-      <u>National Domestic Violence Hotline</u>
+      <a href = "https://www.thehotline.org/"><u>National Domestic Violence Hotline</u></a>
       <br/>1.800.799.7233
       <br/><br/>
-      <u>National Grad Crisis Line</u>
+      <a href = "https://gradresources.org/crisis/?gclid=Cj0KCQjwwJuVBhCAARIsAOPwGATJT0vpnOth2xwUyepNK3Z3Ckjuu8i3uGCay700pUeaih-Moy36o_4aAhLREALw_wcB"><u>National Grad Crisis Line</u></a>
       <br/>1.877.472.3457
       <br/><br/>
-      <u>National Sexual Assault Hotline</u>
+      <a href = "https://www.rainn.org/resources"><u>National Sexual Assault Hotline</u></a>
       <br/>1.800.656.4673
       <br/><br/>
-      <u>National Suicide Prevention Lifeline </u>
-      <br/>1.800.273.8255
-      <br/><br/>
-      <u>Substance Abuse and Mental Health Services Administration National Helpline</u> 
+      <a href = "https://www.samhsa.gov/find-help/national-helpline"><u>Substance Abuse and Mental Health Services Administration National Helpline</u> </a>
       <br/>1.800.662.4357
     </h2>
     </div>
  
-    <a href = "mailto: abc@example.com"><h3 id = 'email'>Email: feedback@euphonics.io</h3></a>
+    <a href = "mailto: feedback@euphonics.io"><h3 id = 'email'>Email: feedback@euphonics.io</h3></a>
 
     <h3 id = 'foot2' >© 2022 DEW</h3>
     </div>
