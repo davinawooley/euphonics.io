d6755fbc43a04899653b45e4b6f304bcd42bc4f0
deeWooley <wooleymediallc@gmail.com>
2022-06-14 21:28:18 -0400

    Button selection

--- src/Emotions.js
+++ src/Emotions.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Emotions.css';
 import './About';
 import './Home';
@@ -6,21 +6,73 @@ import './Load';
 import imageLogo from './static/logo.png'
 import { Helmet } from 'react-helmet';
 import ReactTooltip from 'react-tooltip';
+import { Grid, Box, Button } from "@material-ui/core";
+import { makeStyles, withStyles } from "@material-ui/styles";
 
-function handleClick(e) {
-   e.preventDefault();
-   console.log('The link was clicked.');
- }
+// function handleClick(e) {
+//    e.preventDefault();
+//    console.log('The link was clicked.');
+//  };
+
+
+
+
+ const StyledButton = withStyles(() => ({
+  root: {
+    marginRight: "2rem",
+    marginBottom: "2rem",
+    width: "250px",
+    border: "1px solid #983FE0",
+    padding: "30px",
+    fontSize: "14pt",
+    color: "white",
+    backgroundColor : "black",
+    fontWeight: "300",
+    textTransform: "capitalize"
+  }
+}))(Button);
+
+const useStyles = makeStyles(() => ({
+  buttonContainerWrapper: {
+    display: "flex",
+
+    justifyContent: "center"
+  },
+
+  lastButtonFilter: {
+    marginRight: "0rem"
+  },
+  activeButton: {
+    background: "purple",
+    color: "#fff"
+  }
+}));
+
+
+
+
 export default function Emotions() {
+  
+  const classes = useStyles();
+  const [activeButton, setActiveButton] = useState(false);
+
+  const clickedButtonHandler = (name) => {
+    setActiveButton(name);
+  };
+
+  const buttons = ["Happiness", "Heartbreak", "Confusion", "Contempt", "Family Values" , "Disappointment" ,"Stress" ,"Fear" ,"Jealousy" ,"Forgiveness/Reunion", "Loneliness", "Love" , "Sadness" ,"Self Criticism",  "Compassion"];
+
 
     return (
    
       <div>
- 
-}; 
+
+
+
+
          <Helmet><title>euphonics.io</title>
         </Helmet>
-         <div id = 'container'>
+      <div id = 'container'>
           <a href = './'><img src = {imageLogo} alt = "eupohonics logo" height = {40} id  = "logo"/></a>
           <a href = './about'><h3 id = 'navigation'>About</h3></a>
       </div>
@@ -30,22 +82,44 @@ export default function Emotions() {
            
       <div>
 
-      <button data-tip data-for="registerTip" id = 'happiness' > 
-        <span>Happiness</span></button>
+{/* <Container className={classes.buttonContainerWrapper}> */}
+
+<Box className={classes.buttonContainer}>
+        {buttons.map((name) => (
+          <StyledButton
+            name={name}
+            className={activeButton === name ? `${classes.activeButton}` : ""}
+            onClick={() => clickedButtonHandler(name)}
+          >
+            {name}
+          </StyledButton>
+        ))}
+      </Box>
+
+
+    {/* </Container> */}
+
+      {/* <button data-tip data-for="registerTip" id = 'happiness' >  */}
+        {/* <span>Happiness</span></button> */}
+     
     
-        <button id = 'heartbreak' onClick={handleClick}>
+        {/* <button id = 'heartbreak' onClick={handleClick}> */}
  
- {/* <img src = {imageSpotify} alt = "confusion" height = {27} id = 'spotify'/> */}
+
+
+
+{/* 
+<img src = {imageSpotify} alt = "confusion" height = {27} id = 'spotify'/> 
  <span>Heartbreak</span></button>
 
 <button id = 'confusion' onClick={handleClick}>
 <p data-tip="hello world"></p>
- {/* <img src = {imageSpotify} alt = "confusion" height = {27} id = 'spotify'/> */}
+ <img src = {imageSpotify} alt = "confusion" height = {27} id = 'spotify'/>
  <span>Confusion</span></button>
 
 
 <button id = 'contempt' onClick={handleClick}>
- {/* <img src = {imageSpotify} alt = "contempt" height = {27} id = 'spotify'/> */}
+ <img src = {imageSpotify} alt = "contempt" height = {27} id = 'spotify'/>
  <span>Contempt</span></button>
 
 
@@ -54,60 +128,61 @@ export default function Emotions() {
 
 
 <button id = 'disappointment' onClick={handleClick}>
- {/* <img src = {imageSpotify} alt = "disappointment" height = {27} id = 'spotify'/> */}
+ <img src = {imageSpotify} alt = "disappointment" height = {27} id = 'spotify'/>
  <span>Disappointment</span></button>
 
 
 <button id = 'stress' onClick={handleClick}>
-{/* <img src = {imageSpotify} alt = "stress" height = {27} id = 'spotify'/> */}
+<img src = {imageSpotify} alt = "stress" height = {27} id = 'spotify'/>
 <span>Stress</span></button>
 
 
 <button id = 'fear' onClick={handleClick}>
-{/* <img src = {imageSpotify} alt = "fear" height = {27} id = 'spotify'/> */}
+<img src = {imageSpotify} alt = "fear" height = {27} id = 'spotify'/>
 <span>Fear</span></button>
 
 
 <button id = 'jealousy' onClick={handleClick}>
- {/* <img src = {imageSpotify} alt = "jealousy" height = {27} id = 'spotify'/> */}
+ <img src = {imageSpotify} alt = "jealousy" height = {27} id = 'spotify'/>
  <span>Jealousy</span></button>
 
 
 <button id = 'forgivenessReunion' onClick={handleClick}>
-{/* <img src = {imageSpotify} alt = "forgiveness reunion" height = {27} id = 'spotify'/> */}
+<img src = {imageSpotify} alt = "forgiveness reunion" height = {27} id = 'spotify'/>
 <span>Forgivness/Reunion</span></button>
 
 
 <button id = 'loneliness' onClick={handleClick}>
- {/* <img src = {imageSpotify} alt = "loneliness" height = {27} id = 'spotify'/> */}
+ <img src = {imageSpotify} alt = "loneliness" height = {27} id = 'spotify'/>
  <span>Loneliness</span></button>
 
 
 <button id = 'love' onClick={handleClick}>
- {/* <img src = {imageSpotify} alt = "love" height = {27} id = 'spotify'/> */}
+ <img src = {imageSpotify} alt = "love" height = {27} id = 'spotify'/>
  <span>Love</span></button>
 
 
 <button id = 'sadness' onClick={handleClick}>
- {/* <img src = {imageSpotify} alt = "sadness" height = {27} id = 'spotify'/> */}
+ <img src = {imageSpotify} alt = "sadness" height = {27} id = 'spotify'/>
  <span>Sadness</span></button>
 
 
 <button id = 'selfCritism' onClick={handleClick}>
-{/* <img src = {imageSpotify} alt = "self critism" height = {27} id = 'spotify'/> */}
+<img src = {imageSpotify} alt = "self critism" height = {27} id = 'spotify'/>
 <span>Self Critism</span></button>
 
 
 <button id = 'compassion' onClick={handleClick}>
- {/* <img src = {imageSpotify} alt = "compassion" height = {27} id = 'spotify'/> */}
+ <img src = {imageSpotify} alt = "compassion" height = {27} id = 'spotify'/>
  <span>Compassion</span></button>
 
-    
+     */}
     <a href = './Load'><h1 id = 'makeMix'>make my mix -></h1></a>
     <a href = 'http://www.davinawooley.com' target = 'blank'> <h6 id='footer'>© 2022 DEW</h6></a>
         </div>
         <ReactTooltip />
+        </div> 
         </div>
-        </div>
+        // </div>
       );
     } 
