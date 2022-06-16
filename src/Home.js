5127f66e56a3951de147ccd7e8849e766027fbc9
deeWooley <wooleymediallc@gmail.com>
2022-06-15 22:19:03 -0400

    reset

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,7 @@ import './About';
 import axios from 'axios';
 import { Helmet } from 'react-helmet';
 import ThreeJSHome from './ThreeJSHome';
-import {useEffect, useState} from 'react';
+// import {useEffect, useState} from 'react';
 
 
 function Home() {
@@ -16,53 +16,6 @@ function Home() {
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
   const RESPONSE_TYPE = "token"
 
-  const [token, setToken] = useState("")
-  const [searchKey, setSearchKey] = useState("")
-const [artists, setArtists] = useState([])
-
-
-const renderArtists = () => {
-  return artists.map(artist => (
-      <div key={artist.id}>
-          {artist.images.length ? <img width={"100%"} src={artist.images[0].url} alt=""/> : <div>No Image</div>}
-          {artist.name}
-      </div>
-  ))
-}
-
-  useEffect(() => {
-    const hash = window.location.hash
-    let token = window.localStorage.getItem("token")
-
-    if (!token && hash) {
-        token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
-
-        window.location.hash = ""
-        window.localStorage.setItem("token", token)
-    }
-
-    setToken(token)
-
-}, [])
-
-const logout = () => {
-  setToken("")
-  window.localStorage.removeItem("token")
-}
-const searchArtists = async (e) => {
-  e.preventDefault()
-  const {data} = await axios.get("https://api.spotify.com/v1/search", {
-      headers: {
-          Authorization: `Bearer ${token}`
-      },
-      params: {
-          q: searchKey,
-          type: "artist"
-      }
-  })
-
-  setArtists(data.artists.items)
-}
 
   return (
 
@@ -82,7 +35,7 @@ const searchArtists = async (e) => {
 <div id = "fullbody">
       <Helmet><title>euphonics.io</title>
       </Helmet>
-      <button onClick={logout}>Logout</button>}
+
       <div id = "container">
       <a href = './'><img src = {imageLogo} alt = "eupohonics logo" height = {40} id  = "logo"/></a>
         <a href = './about'>  <h3 id = "navigation">About</h3></a>
@@ -100,16 +53,8 @@ const searchArtists = async (e) => {
       <span>Access with Spotify</span>
      
       </button>
-
       </a>
-      <form onSubmit={searchArtists}>
-    <input type="text" onChange={e => setSearchKey(e.target.value)}/>
-    <button type={"submit"}>Search</button>
-</form>
-
-
       </div>
-      {renderArtists()}
       </div>
       <h3 id = 'foot' >© 2022 DEW</h3>
  
